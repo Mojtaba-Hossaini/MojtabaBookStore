94edbee80d4272c71e7a7fc168d6455b3ad3f1bb
MWW1987 <s.mojtaba1987@gmail.com>
2019-05-15 13:40:58 +0430

    25.2.98.1

---
+++ MojtabaBookStore/MojtabaBookStore/Models/Author_Book.cs
@@ -0,0 +1,12 @@
+﻿namespace MojtabaBookStore.Models
+{
+    public class Author_Book
+    {
+        public int Author_BookID { get; set; }
+        public int BookID { get; set; }
+        public int AuthorID { get; set; }
+
+        public Book Book { get; set; }
+        public Author Author { get; set; }
+    }
+}
---
+++ MojtabaBookStore/MojtabaBookStore/Models/Book.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MojtabaBookStore.Models
+{
+    [Table("BookInfo")]
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+        public string Summary { get; set; }
+        public int Price { get; set; }
+        public int Stock { get; set; }
+        public string File { get; set; }
+
+        [Column(TypeName = "image")]
+        public byte[] Image { get; set; }
+        public int LanguageID { get; set; }
+
+        [ForeignKey("SubCategory")]
+        public int SCategoryID { get; set; }
+
+        public SubCategory SubCategory { get; set; }
+        public Language Language { get; set; }
+
+        public Discount Discount { get; set; }
+        public List<Author_Book> Author_Books { get; set; }
+        public List<Order_Book> Order_Books { get; set; }
+    }
+}
